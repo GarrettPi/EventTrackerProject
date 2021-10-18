d7f70aaa5c6086f6c1a2ef1b9371810e3583e417
GarrettPi <86500870+GarrettPi@users.noreply.github.com>
2021-10-18 01:25:45 -0400

    final changes

---
+++ 3dPrintTracker/src/main/java/com/skilldistillery/printtracker/controllers/PrinterTypeController.java
@@ -0,0 +1,24 @@
+package com.skilldistillery.printtracker.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.skilldistillery.printtracker.entities.PrinterType;
+import com.skilldistillery.printtracker.services.PrinterTypeService;
+
+@RestController
+@RequestMapping("api")
+public class PrinterTypeController {
+
+	@Autowired
+	PrinterTypeService printerTypeSvc;
+
+	@GetMapping("types")
+	public List<PrinterType> index(){ 
+		return printerTypeSvc.getAllPrinterTypes();
+	}
+}
--- 3dPrintTracker/src/main/resources/static/js/script.js
+++ 3dPrintTracker/src/main/resources/static/js/script.js
@@ -5,21 +5,12 @@ var init = function(e){
 	xhr.onreadystatechange = function(){
 		if(xhr.status < 400 && xhr.readyState === 4){
 			var data = JSON.parse(xhr.responseText);
-			console.log(data);
 			displayAllPrints(data);
-
-			var xhr2 = new XMLHttpRequest();
-			xhr2.open('GET', 'api/printers');
-			xhr2.onreadystatechange = function(){
-				if(xhr2.status < 400 && xhr2.readyState === 4){
-					printersList = JSON.parse(xhr2.responseText);
-					loadPrintersList(printersList);
-				}
-				else if (xhr2.readyState === 4 && xhr2.status >= 400){
-					console.log('***********************Some Error****************');
-				}
-			}
-			xhr2.send(null);
+			console.log('loading printers');
+			getPrinters();
+			console.log('loading types');
+			getPrinterTypes();
+			// getMaterials();
 		}
 		else if (xhr.readyState === 4 && xhr.status >= 400) {
 			console.log('******************some Error*********************');
@@ -28,13 +19,45 @@ var init = function(e){
 	xhr.send(null);
 
 };
+function getPrinters() {
+	var xhr2 = new XMLHttpRequest();
+	xhr2.open('GET', 'api/printers');
+	xhr2.onreadystatechange = function(){
+		if(xhr2.status < 400 && xhr2.readyState === 4){
+			printersList = JSON.parse(xhr2.responseText);
+			loadPrintersList(printersList);
+		}
+		else if (xhr2.readyState === 4 && xhr2.status >= 400){
+			console.log('***********************Some Error****************');
+		}
+	}
+	xhr2.send(null);
+}
+
+function getPrinterTypes() {
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', 'api/types');
+	xhr.onreadystatechange = function(){
+		if(xhr.status < 400 && xhr.readyState === 4){
+			var data = JSON.parse(xhr.responseText);
+			loadPrinterTypes(data);
+		}
+		else if (xhr.readyState === 4 && xhr2.status >= 400){
+			console.log('***********************Some Error****************');
+		}
+	}
+	xhr.send(null);
+}
 
 window.addEventListener('load', init);
 var printersList;
 var printerSelect = document.getElementById('printerSelect');
+var printerTypeSelect = document.getElementById('printerTypeSelect');
+
 var listItems = document.getElementsByClassName('lr');
 var singleResult = document.getElementById('singleResult');
 var newPrintForm = document.newPrintForm;
+var newPrinterForm = document.newPrinterForm;
 
 function loadPrintersList(printers){
 	for(let printer of printers){
@@ -42,7 +65,16 @@ function loadPrintersList(printers){
 		option.value = printer.id;
 		option.textContent = printer.name;
 		printerSelect.appendChild(option);
-		console.log(printer);
+	}
+}
+
+function loadPrinterTypes(types){
+	console.log(types);
+	for(let type of types){
+		let option = document.createElement('option');
+		option.value = type.id;
+		option.textContent = type.name;
+		printerTypeSelect.appendChild(option);
 	}
 }
 
@@ -58,7 +90,7 @@ var addPrint = function(e){
 			var data = JSON.parse(xhr.responseText);
 			console.log('ambiguous log');
 			console.log(data);
-			if(alert('New Print Created Successfully!')){}
+			if(alert('Print created successfully!')){}
 			else window.location.reload();
 		}
 		else if(xhr.readyState === 4 && xhr.status >= 400) {
@@ -89,6 +121,8 @@ var addPrint = function(e){
 	xhr.send(newPrintJson);
 
 };
+
+//huge function to populate the details form and allow for edits and deletes
 
 var itemClick = function(e){
 	let detail = document.getElementById('singleResult');
@@ -135,6 +169,7 @@ var itemClick = function(e){
 	input = document.createElement('input');
 	input.setAttribute('type', 'text');
 	input.setAttribute('name', 'materialConsumed');
+	input.setAttribute('required', true);
 	input.setAttribute('value', this.firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling.textContent);
 	p = document.createElement('p');
 	p.textContent = 'Material Consumed: ';
@@ -142,9 +177,23 @@ var itemClick = function(e){
 	form.appendChild(p);
 	// form.appendChild(document.createElement('br'));
 
+	select = document.createElement('select');
+	select.setAttribute('name', 'printer');
+	for(let printer of printersList){
+		let option = document.createElement('option');
+		option.value = printer.id;
+		option.textContent = printer.name;
+		select.appendChild(option);
+	}
+	p = document.createElement('p');
+	p.textContent = 'Printer: ';
+	p.appendChild(select);
+	form.appendChild(p);
+
 	input = document.createElement('input');
 	input.setAttribute('type', 'text');
 	input.setAttribute('name', 'duration');
+	input.setAttribute('required', true);
 	input.setAttribute('value', this.firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.textContent);
 	p = document.createElement('p');
 	p.textContent = 'Print Duration: ';
@@ -177,16 +226,13 @@ var itemClick = function(e){
 			}
 		};
 		var editedPrint = {
-    	"id": form.id.value,
+			"id": form.id.value,
     	"name": form.name.value,
     	"duration": form.duration.value,
     	"materialConsumed": form.materialConsumed.value,
     	"photoUrl": form.photoUrl.value,
     	"printer": {
-        	"id": 1,
-        	"printerType": {
-            	"id": 1,
-        	}
+        	"id": form.printer.value
     	},
     	"source": {
         	"id": 1
@@ -235,6 +281,8 @@ var itemClick = function(e){
 	this.style.backgroundColor = 'rgb(220, 220, 220)';
 };
 
+//Display all prints when page loads
+
 function displayAllPrints(data){
 	var resultsDiv = document.getElementById('printsResults');
 	let table = document.createElement('table');
@@ -249,10 +297,10 @@ function displayAllPrints(data){
 	h.textContent = 'Name';
 	row.appendChild(h);
 	h = document.createElement('th');
-	h.textContent = "Material Used";
+	h.textContent = "Material Used (Kilograms)";
 	row.appendChild(h);
 	h = document.createElement('th');
-	h.textContent = 'Print Duration';
+	h.textContent = 'Print Duration (Minutes)';
 	row.appendChild(h);
 	h = document.createElement('th');
 	h.textContent = 'Cost of Material';
@@ -280,10 +328,10 @@ function displayAllPrints(data){
 		d.textContent = item.name;
 		row.appendChild(d);
 		d = document.createElement('td');
-		d.textContent = item.materialConsumed+' kg';
+		d.textContent = item.materialConsumed;
 		row.appendChild(d);
 		d = document.createElement('td');
-		d.textContent = item.duration > 60 ? Math.round((item.duration/60) * 100) /100 +' hours' : item.duration +' minutes';
+		d.textContent = item.duration;
 		row.appendChild(d);
 		d = document.createElement('td');
 		d.textContent = '$' + Math.round((item.materialConsumed * item.material.cost) * 100) /100;
@@ -301,4 +349,31 @@ function displayAllPrints(data){
 	resultsDiv.appendChild(table);
 }
 
+var addPrinter = function(e){
+	e.preventDefault();
+	var xhr = new XMLHttpRequest();
+	xhr.open('POST', 'api/printers/');
+	xhr.setRequestHeader("Content-type", "application/json");
+	xhr.onreadystatechange = function(){
+		if(xhr.readyState === 4 && (xhr.status === 200 || xhr.status === 201)){
+			var data = JSON.parse(xhr.responseText);
+			if(alert('New Printer Created Successfully')){}
+			else window.location.reload();
+		}
+		else if (xhr.readyState === 4 && xhr.status >= 400){
+			console.log('Printer Creation failed');
+		}
+	};
+
+	var newPrinter = {
+		'name': newPrinterForm.name.value,
+		'printerType': {
+			'id': newPrinterForm.printerType.value
+		}
+	};
+	var newPrinterJson = JSON.stringify(newPrinter);
+	xhr.send(newPrinterJson);
+};
+
 newPrintForm.submit.addEventListener('click', addPrint);
+newPrinterForm.submit.addEventListener('click', addPrinter);
